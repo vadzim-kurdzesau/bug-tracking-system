47dd5f0c73e13d588c757d2163dae7f5974012ca
Vadzim Kurdzesau <Vadzim_Kurdzesau@epam.com>
2021-11-18 11:16:15 +0300

    feat: implement models

---
+++ BugTrackingSystem/Models/BugStatus.cs
@@ -0,0 +1,11 @@
+﻿namespace BugTrackingSystem.Models
+{
+    public class BugStatus
+    {
+        public int Id { get; set; }
+
+        public string Status { get; set; }
+
+        public string Description { get; set; }
+    }
+}
---
+++ BugTrackingSystem/Models/BugType.cs
@@ -0,0 +1,11 @@
+﻿namespace BugTrackingSystem.Models
+{
+    public class BugType
+    {
+        public int Id { get; set; }
+
+        public string Type { get; set; }
+
+        public string Description { get; set; }
+    }
+}
