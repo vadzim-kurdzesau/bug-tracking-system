693517216d34ce5e0cad29a135268df1c67c331b
Vadzim Kurdzesau <Vadzim_Kurdzesau@epam.com>
2021-11-18 11:29:44 +0300

    feat: initialize context

---
+++ BugTrackingSystem/BugTrackingSystemContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BugTrackingSystem.Models;
+
+namespace BugTrackingSystem
+{
+    public class BugTrackingSystemContext : DbContext
+    {
+        public DbSet<Project> Projects { get; set; }
+
+        public DbSet<Developer> Developers { get; set; }
+
+        public DbSet<ProjectsDevelopers> ProjectsDevelopers { get; set; }
+
+        public DbSet<BugType> BugTypes { get; set; }
+
+        public DbSet<BugStatus> BugStatuses { get; set; }
+
+        public DbSet<Bug> Bugs { get; set; }
+
+        public DbSet<BugHistoryRecord> BugHistory { get; set; }
+    }
+}
