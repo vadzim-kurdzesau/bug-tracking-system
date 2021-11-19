04232497891e651271bbc294d96fd750bf623520
Vadzim Kurdzesau <Vadzim_Kurdzesau@epam.com>
2021-11-19 16:21:26 +0300

    feat: implement CRUD methods for Projects

---
+++ BugTrackingSystem/Extensions/ProjectsRepositoryExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BugTrackingSystem.Models;
+
+namespace BugTrackingSystem.Extensions
+{
+    internal static class ProjectsRepositoryExtensions
+    {
+        internal static void UpdateProjectTo(this Project projectToUpdate, Project update)
+        {
+            (projectToUpdate.Name, projectToUpdate.StartDate) = (update.Name, update.StartDate);
+        }
+    }
+}
---
+++ BugTrackingSystem/ProjectsRepository.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using BugTrackingSystem.Extensions;
+using BugTrackingSystem.Models;
+
+namespace BugTrackingSystem
+{
+    public class ProjectsRepository
+    {
+        private readonly BugTrackingSystemContext context;
+
+        public ProjectsRepository(BugTrackingSystemContext context)
+            => this.context = context;
+
+        public void AddProject(Project project)
+        {
+            this.context.Projects.Add(project);
+            this.context.SaveChanges();
+        }
+
+        public Project FindProject(int id)
+            => this.context.Projects.Find(id);
+
+        public void UpdateProject(Project project)
+        {
+            if (project is null)
+            {
+                throw new ArgumentNullException(nameof(project));
+            }
+
+            var projectToUpdate = this.context.Projects.Find(project.Id);
+            projectToUpdate?.UpdateProjectTo(project);
+
+            this.context.SaveChanges();
+        }
+
+        public void DeleteProject(int id)
+        {
+            var projectToDelete = this.context.Projects.First(p => p.Id == id);
+            this.context.Projects.Remove(projectToDelete);
+
+            this.context.SaveChanges();
+        }
+    }
+}
