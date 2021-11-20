61c9acc37940cc0335de405f9f3582ad4a065494
Vadzim Kurdzesau <Vadzim_Kurdzesau@epam.com>
2021-11-20 23:43:12 +0300

    feat: implement GetUnassignedDevelopers

--- BugTrackingSystem.Services/DevelopersService.cs
+++ BugTrackingSystem.Services/DevelopersService.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Data.Entity;
+using System.Linq;
 using BugTrackingSystem.Models;
 
 namespace BugTrackingSystem.Services
@@ -15,6 +17,24 @@ namespace BugTrackingSystem.Services
 
             context.Developers.Add(developer ?? throw new ArgumentNullException(nameof(developer)));
             return context;
+        }
+
+        public static IEnumerable<Developer> GetUnassignedDevelopers(this BugTrackingSystemContext context)
+        {
+            if (context is null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            var query = from   developer
+                                           in     context.Developers.Include(d => d.Bugs)
+                                           where  developer.Bugs.Count == 0
+                                           select developer;
+
+            foreach (var developer in query)
+            {
+                yield return developer;
+            }
         }
     }
 }
