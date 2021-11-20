108ef8a86aea6b001bcb3bf736d8665ce4efe990
Vadzim Kurdzesau <Vadzim_Kurdzesau@epam.com>
2021-11-20 23:19:13 +0300

    feat: implement AddDeveloper() method

--- BugTrackingSystem.Services/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace BugTrackingSystem.Services
-{
-    public class Class1
-    {
-    }
-}
---
+++ BugTrackingSystem.Services/DevelopersService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Data.Entity;
+using BugTrackingSystem.Models;
+
+namespace BugTrackingSystem.Services
+{
+    public static class DevelopersService
+    {
+        public static DbContext AddDeveloper(this BugTrackingSystemContext context, Developer developer)
+        {
+            if (context is null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            context.Developers.Add(developer ?? throw new ArgumentNullException(nameof(developer)));
+            return context;
+        }
+    }
+}
