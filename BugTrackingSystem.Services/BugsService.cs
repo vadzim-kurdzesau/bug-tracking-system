22c157974e10aaa343d12086bc360003286f7199
Vadzim Kurdzesau <Vadzim_Kurdzesau@epam.com>
2021-11-20 23:21:19 +0300

    feat: implement AssignDeveloper() method

---
+++ BugTrackingSystem.Services/BugsService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using BugTrackingSystem.Models;
+
+namespace BugTrackingSystem.Services
+{
+    public static class BugsService
+    {
+        public static Bug AssignDeveloper(this Bug bug, Developer developer)
+        {
+            if (bug is null)
+            {
+                throw new ArgumentNullException(nameof(bug));
+            }
+
+            bug.Developer = developer ?? throw new ArgumentNullException(nameof(developer));
+            return bug;
+        }
+    }
+}
