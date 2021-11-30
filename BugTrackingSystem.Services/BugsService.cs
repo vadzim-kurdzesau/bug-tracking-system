da77b1c60c6becbffb418b5c22fd4b44d982ba99
Vadzim Kurdzesau <Vadzim_Kurdzesau@epam.com>
2021-11-30 18:03:43 +0300

    refactor: extract generic validation method; add editorconfig

--- BugTrackingSystem.Services/BugsService.cs
+++ BugTrackingSystem.Services/BugsService.cs
@@ -10,37 +10,28 @@ namespace BugTrackingSystem.Services
     {
         public static Bug AssignDeveloper(this Bug bug, Developer developer)
         {
-            ValidateBug(bug);
-
-            //consider creation of the generic extension method for ArgumentNullException validation
-            bug.Developer = developer ?? throw new ArgumentNullException(nameof(developer));
+            bug.ValidateNotNull();
+            bug.Developer = developer.ValidateNotNull();
             return bug;
         }
 
         public static Bug UnassignDeveloper(this Bug bug)
         {
-            ValidateBug(bug);
+            bug.ValidateNotNull();
             bug.Developer = null;
             return bug;
         }
 
         public static IEnumerable<Bug> GetUnassignedBugs(this Developer developer, BugTrackingSystemContext context)
         {
-            ValidateContext(context);
+            context.ValidateNotNull();
 
-            // var unassignedBugs =   from p
-            //                                         in developer.Projects
-            //                                       join b in context.Bugs
-            //                                         on p.Id equals b.ProjectId
-            //                                      where b.DeveloperId is null
-            //                                     select b;
-
-            // todo: let's discuss which syntax is preferable 
-            var unassignedBugs = developer
-                                                .Projects
-                                                .SelectMany(p => p.Bugs)
-                                                .Where(b => !b.DeveloperId.HasValue)
-                                                .ToList();
+            var unassignedBugs = from p
+                                                  in developer.Projects
+                                                join b in context.Bugs
+                                                  on p.Id equals b.ProjectId
+                                               where b.DeveloperId is null
+                                              select b;
 
             foreach (var bug in unassignedBugs)
             {
@@ -64,19 +55,15 @@ namespace BugTrackingSystem.Services
 
         public static Bug SetBugStatus(this Bug bug, BugTrackingSystemContext context, string statusName)
         {
+            statusName.ValidateNotNull();
             ValidateContextAndBug(context, bug);
-            ValidateStatusName(statusName);
 
-            // var status = (  from s 
-            //                   in context.BugStatuses 
-            //                where s.Status == statusName
-            //               select s).First();
-            
-            var status = context
-                            .BugStatuses
-                            .First(s => s.Status == statusName);
+             var status = (  from s 
+                               in context.BugStatuses 
+                            where s.Status == statusName
+                           select s).FirstOrDefault();
 
-            bug.BugStatus = status ?? throw new ArgumentException($"There is no such status: {statusName}.");
+            bug.BugStatus = status.ValidateNotNull(message: $"There is no such status: {statusName}.");
             bug.UpdateDate = DateTime.Now;
             return bug;
         }
@@ -86,46 +73,25 @@ namespace BugTrackingSystem.Services
             ValidateContextAndBug(context, bug);
             context.SaveChanges();
 
-            var record = (  from r
-                              in context.BugsAudit
-                           where r.BugId == bug.Id
+            var record = (from r
+                            in context.BugsAudit
+                          where r.BugId == bug.Id
                           select r).Last();
 
-            record.DeveloperMessage = message;
+            record.DeveloperMessage = message.ValidateNotNull();
             return bug;
         }
 
         private static void ValidateContextAndBug(DbContext context, Bug bug)
         {
-            ValidateContext(context);
-            ValidateBug(bug);
-        }
-
-        private static void ValidateBug(Bug bug)
-        {
-            bug = bug ?? throw new ArgumentNullException(nameof(bug));
+            context.ValidateNotNull();
+            bug.ValidateNotNull();
         }
 
         private static void ValidateBugs(Bug bug, Bug update)
         {
-            ValidateBug(bug);
-            ValidateBug(update);
-        }
-
-        private static void ValidateContext(DbContext context)
-        {
-            if (context is null)
-            {
-                throw new ArgumentNullException(nameof(context));
-            }
-        }
-
-        private static void ValidateStatusName(string statusName)
-        {
-            if (string.IsNullOrWhiteSpace(statusName))
-            {
-                throw new ArgumentNullException(statusName, "Status name is null, empty or a whitespace.");
-            }
+            bug.ValidateNotNull();
+            update.ValidateNotNull();
         }
     }
 }
--- BugTrackingSystem/BugTrackingSystemContext.cs
+++ BugTrackingSystem/BugTrackingSystemContext.cs
@@ -21,5 +21,36 @@ namespace BugTrackingSystem
         public DbSet<Bug> Bugs { get; set; }
 
         public DbSet<BugsAuditRecord> BugsAudit { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //modelBuilder.Entity<Project>()
+            //    .HasMany(p => p.Developers)
+            //    .WithMany(d => d.Projects)
+            //    .Map(c =>
+            //    {
+            //        c.MapLeftKey("project_id");
+            //        c.MapRightKey("developer_id");
+            //        c.ToTable("projects_developers");
+            //    });
+
+            //modelBuilder.Entity<Project>()
+            //    .HasMany(p => p.Bugs)
+            //    .WithRequired(b => b.Project);
+
+            //modelBuilder.Entity<BugStatus>()
+            //    .HasMany(s => s.Bugs)
+            //    .WithRequired(b => b.BugStatus);
+
+            //modelBuilder.Entity<BugType>()
+            //    .HasMany(t => t.Bugs)
+            //    .WithRequired(b => b.BugType);
+
+            //modelBuilder.Entity<Developer>()
+            //    .HasMany(d => d.Bugs)
+            //    .WithOptional(b => b.Developer);
+        }
     }
 }
